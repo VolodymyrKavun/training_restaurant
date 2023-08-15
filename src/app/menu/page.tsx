bf9b7ff7d41af64cc75a8dcf6e08f61785db33db
Webevery <webforevery@gmail.com>
2023-08-15 14:56:22 +0300

    change menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -11,12 +11,13 @@ const getData = async () => {
     throw new Error("Failed!");
   }
 
-  return res.json();
+  const data = await res.json();
+
+  return data;
 };
 
-const menu: MenuType = await getData();
-
-const MenuPage = () => {
+const MenuPage = async () => {
+  const menu: MenuType = await getData();
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
